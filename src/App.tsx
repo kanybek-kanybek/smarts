8cd64d19e529b35ff72e371d5696e34c74732e6c
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-04-01 15:39:51 +0600

    .

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/home";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import DetalProduct from "./pages/DetalProduct/DetalProduct";
+import GroupProduct from "./pages/groupProduct/GroupProduct";
+import Review from "./pages/review/review";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/detal/product",
+        element: <DetalProduct />,
+    },
+    {
+        path: "/group/product",
+        element: <GroupProduct />,
+    },
+    {
+        path: "/review",
+        element: <Review />,
+    },
+]);
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <RouterProvider router={router} />
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/groupProduct/GroupProduct.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GroupProduct() {
+    return <div>GroupProduct</div>;
+}
+
+export default GroupProduct;
