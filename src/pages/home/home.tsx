898a6d7e2acd0692738e967bc05cac09a0701098
saadat <saadat0989@gmail.com>
2024-04-02 18:32:34 +0600

    asdf

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import HomeAdvertising from "./homeAdvertising";
 // import New from "./New";
+import Hero from "../../components/hero/hero";
 // import Hero from "../../components/hero/hero";
 import HomeReview from "./homeReview";
 import HomeAdvertising from "../../../src/components/homeVenera/homeAdvertising";
