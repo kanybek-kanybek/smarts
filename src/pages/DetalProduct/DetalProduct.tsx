860dc00fa806124fa61cd051d5768ff77dab1d71
MOTION_NOUT <veneraajbekova569@gmail.com>
2024-04-02 16:40:55 +0600

    xzz

--- src/pages/DetalProduct/DetalProduct.tsx
+++ src/pages/DetalProduct/DetalProduct.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function DetalProduct() {
-    return <div>DetalProduct</div>;
+  return <div>DetalProduct</div>;
 }
 
 export default DetalProduct;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Hero from "../../components/hero/hero";
+// import HomeAdvertising from "./homeAdvertising";
+// import New from "./New";
+// import Hero from "../../components/hero/hero";
 import HomeReview from "./homeReview";
 
 function Home() {
   return (
     <>
-      {/* <HomeAdvertising /> */}
+      <HomeReview />
       {/* <New /> */}
-      <section>
-        <div className="container">
-          <Hero />
-          {/* <HomeReview /> */}
-        </div>
-      </section>
+      {/* <HomeReview /> */}
+      fhewo
     </>
   );
 }
+
 export default Home;
