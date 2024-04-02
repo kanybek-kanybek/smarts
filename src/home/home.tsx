3aca5953c68ecf26367867a38c13eb0506ec54f8
MOTION_NOUT <veneraajbekova569@gmail.com>
2024-04-02 16:48:45 +0600

    qwertyuio

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 // import HomeAdvertising from "./homeAdvertising";
 // import New from "./New";
-import Hero from "../../components/hero/hero";
 // import Hero from "../../components/hero/hero";
 import HomeReview from "./homeReview";
-import HomeAdvertising from "../../../src/components/homeVenera/homeAdvertising";
-import New from "../../components/homeVenera/New";
-import New from "../../../src/";
+
 function Home() {
   return (
     <>
       <HomeReview />
-      <New />
-      <HomeAdvertising />
       {/* <New /> */}
       {/* <HomeReview /> */}
+      fhewo
     </>
   );
 }
