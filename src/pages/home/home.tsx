cbdbde11ef7eb470b8bb65bcde92a1275c796ff1
saadat <saadat0989@gmail.com>
2024-04-02 11:31:41 +0600

    ...

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HomeReview from "./homeReview";
 
 function Home() {
-    return <div>Home</div>;
+  return (
+    <section>
+      <div className="container">
+        <HomeReview />
+        
+      </div>
+    </section>
+  );
 }
 
 export default Home;
